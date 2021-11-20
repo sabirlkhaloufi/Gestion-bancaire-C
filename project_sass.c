dfca03f81ca57e6e1a7261560dd28db149ee2a19
sabir lkhaloufi <khalloufisabir7@gmail.com>
2021-11-20 00:21:57 -0800

    creation la fonction de menu et de  saisir les information des compte

---
+++ project_sass.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+/* declarer la structure pour facélité le saisir 
+les information de plusieur comptes*/
+struct InfoClients{
+    char CIN[10];
+    char Nom[10];
+    char Prenom[10];
+    double Montant;
+};
+/* fonction de saisir les infomations de compte*/
+void    SaisirInfoClients(){
+    struct InfoClients info;
+    printf("entrer le CIN: ");
+    scanf("%s",info.CIN);
+    printf("entrer le Nom: ");
+    scanf("%s",info.Nom);
+    printf("entrer le Prenom: ");
+    scanf("%s",info.Prenom);
+    printf("entrer le Montant: ");
+    scanf("%lf",&info.Montant);
+}
+/* fonction pour ajouter un neuveau compte*/
+void    AjouterCompte(){
+    printf("\n___Introduire un compte bancaire___\n\n");
+    printf("__veuille saisir les informations__\n\n");
+    SaisirInfoClients();
+}
+/* fonction pour ajouter plusieurs compte*/
+void    AjouterPlusieurCompte(){
+    int NbrClients,i;
+    printf("\n___Introduire plusieurs compte bancaire___\n\n");
+    printf("saisir le nombre des comptes que vou avez ajouter: \n");
+    scanf("%d",&NbrClients);
+    for (i = 1; i <= NbrClients; i++)
+    {
+        printf("\nveuille saisir les informations numero %d: \n\n",i);
+        SaisirInfoClients();
+    }
+}
+/* fonction pour afficher le menu principal de l'application*/
+void    MenuPrincipal(){
+    int choix;
+    printf("\n\n**l\'application de Gestion de Bancaire**\n\n");
+    do
+    {
+        
+        printf("\n\n_1_Introduire un compte bancaire_________\n");
+        printf("\n_2_Introduire plusieurs comptes bancaires\n");
+        printf("\n_3_Operations____________________________\n");
+        printf("\n_4_Affichage_____________________________\n");
+        printf("\n_5_Fidelisation__________________________\n");
+        printf("\n_6_Quitter l\'application_________________\n");
+        printf("\n__________donner votre choix ____________\n");
+        scanf("%d",&choix);
+
+        switch (choix)
+        {
+        case 1:
+            AjouterCompte();
+            break;
+        case 2:
+            AjouterPlusieurCompte();
+            break;
+        case 3:
+    
+            break;
+        case 4:
+            break;
+        case 5:
+            /* code */
+            break;
+        case 6:
+            printf("\n\nvous avez quitte le programme:");
+            break;
+        default:
+            printf("erreur de saisir  :\n");
+            break;
+        }
+    } while (choix != 6);
+    
+}
+
+int main(){
+    MenuPrincipal();
+    return 0;
+}
